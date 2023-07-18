e64866a2e8fa3962c000295aa950d34439002c66
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-07-18 22:06:06 +0200

    2h

---
+++ src/main/scala/part2datastreams/WindowFunctions.scala
@@ -0,0 +1,104 @@
+package part2datastreams
+
+import generators.gaming._
+import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.{AllWindowFunction, ProcessAllWindowFunction}
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+import java.time.Instant
+import scala.concurrent.duration._
+
+object WindowFunctions {
+
+
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  implicit val serverStartTime: Instant =
+    Instant.parse("2023-07-17T00:00:00.000Z")
+
+  val events: List[ServerEvent] = List(
+    bob.register(2.seconds),
+    bob.online(2.seconds),
+    sam.register(3.seconds),
+    sam.online(4.seconds),
+    rob.register(4.seconds),
+    alice.register(4.seconds),
+    mary.register(6.seconds),
+    mary.online(6.seconds),
+    carl.register(8.seconds),
+    rob.online(10.seconds),
+    alice.online(10.seconds),
+    carl.online(10.seconds)
+  )
+
+  val eventStream: DataStream[ServerEvent] = env
+    .fromCollection(events)
+    .assignTimestampsAndWatermarks(
+      WatermarkStrategy
+        .forBoundedOutOfOrderness(java.time.Duration.ofMillis(500))
+        .withTimestampAssigner(new SerializableTimestampAssigner[ServerEvent] {
+          override def extractTimestamp(element: ServerEvent, recordTimestamp: Long): Long =
+            element.eventTime.toEpochMilli
+        })
+    )
+
+  // how many players were registerd every 3 seconds?
+  // [0s...3s] [3s...6s] [6s...9s]
+
+  val threeSecondsTumblingWindow: AllWindowedStream[ServerEvent, TimeWindow] = eventStream.windowAll(TumblingEventTimeWindows.of(Time.seconds(3)))
+
+  class CountByWindowAll extends AllWindowFunction[ServerEvent, String, TimeWindow] {
+    override def apply(window: TimeWindow, input: Iterable[ServerEvent], out: Collector[String]): Unit = {
+      val registrationEventCount = input.count(event => event.isInstanceOf[PlayerRegistered])
+      out.collect(s"Window [${window.getStart} - ${window.getEnd}] $registrationEventCount")
+    }
+  }
+
+  def demoCountByWindow(): Unit = {
+    val registrationsPerThreeSeconds = threeSecondsTumblingWindow.apply(new CountByWindowAll)
+    registrationsPerThreeSeconds.print()
+    env.execute()
+  }
+
+  class CountByWindowAll_v2 extends ProcessAllWindowFunction[ServerEvent, String, TimeWindow] {
+    override def process(context: Context, elements: Iterable[ServerEvent], out: Collector[String]): Unit = {
+      val window = context.window
+      val registrationEventCount = elements.count(event => event.isInstanceOf[PlayerRegistered])
+      out.collect(s"Window [${window.getStart} - ${window.getEnd}] $registrationEventCount")
+    }
+  }
+
+  def demoCountByWindow_v2(): Unit = {
+    val registrationsPerThreeSeconds = threeSecondsTumblingWindow.process(new CountByWindowAll_v2)
+    registrationsPerThreeSeconds.print()
+    env.execute()
+  }
+
+  class CountByWindow_v3 extends AggregateFunction[ServerEvent, Long, Long] {
+    override def createAccumulator(): Long = 0L
+
+    override def add(value: ServerEvent, accumulator: Long): Long =
+      if (value.isInstanceOf[PlayerRegistered]) accumulator +1
+      else accumulator
+
+    override def getResult(accumulator: Long): Long = accumulator
+
+    override def merge(a: Long, b: Long): Long = a + b
+  }
+
+  def demoCountByWindow_v3(): Unit  = {
+    val registrationsPerThreeSeconds = threeSecondsTumblingWindow.aggregate(new CountByWindow_v3)
+    registrationsPerThreeSeconds.print()
+    env.execute()
+  }
+
+  def main(args: Array[String]): Unit = {
+    demoCountByWindow_v3()
+  }
+
+}
