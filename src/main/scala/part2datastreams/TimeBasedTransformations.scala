f690f7e6342904d70ac66691330fa6d129fef73a
Marcin Szaroszyk <marcin.szaroszyk@gmail.com>
2023-07-21 20:08:02 +0200

    1h

---
+++ src/main/scala/part2datastreams/TimeBasedTransformations.scala
@@ -0,0 +1,65 @@
+package part2datastreams
+
+import generators.gaming.{PlayerRegistered, ServerEvent}
+import generators.shopping._
+import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.ProcessAllWindowFunction
+import org.apache.flink.streaming.api.windowing.assigners.{TumblingEventTimeWindows, TumblingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+import java.time.Instant
+
+object TimeBasedTransformations {
+
+  val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+  val shoppingCartEvents: DataStream[ShoppingCartEvent] = env.addSource(
+    new ShoppingCartEventsGenerator(
+      sleepMillisPerEvent = 100, batchSize = 5, baseInstant = Instant.parse("2023-07-21T00:00:00.000Z")
+    )
+  )
+
+  // 1. Event-time = the moment the event was created
+  // 2. Processing-time = the moment the event ARRIVES at flink
+
+  class CountByWindowAll extends ProcessAllWindowFunction[ShoppingCartEvent, String, TimeWindow] {
+    override def process(context: Context, elements: Iterable[ShoppingCartEvent], out: Collector[String]): Unit = {
+      val window = context.window
+      out.collect(s"Window [${window.getStart} - ${window.getEnd}] ${elements.size}")
+    }
+  }
+
+  def demoProcessingTime(): Unit = {
+    def groupedEventsByWindow: AllWindowedStream[ShoppingCartEvent, TimeWindow] = shoppingCartEvents.windowAll(TumblingProcessingTimeWindows.of(Time.seconds(3)))
+    def countEventsByWindow = groupedEventsByWindow.process(new CountByWindowAll)
+    countEventsByWindow.print()
+    env.execute()
+  }
+
+  def demoEventTime(): Unit = {
+    val groupedEventsByWindow = shoppingCartEvents
+      .assignTimestampsAndWatermarks(
+        WatermarkStrategy
+          .forBoundedOutOfOrderness(java.time.Duration.ofMillis(500))
+          .withTimestampAssigner(new SerializableTimestampAssigner[ShoppingCartEvent] {
+            override def extractTimestamp(element: ShoppingCartEvent, recordTimestamp: Long): Long = element.time.toEpochMilli
+          })
+      )
+      .windowAll(TumblingEventTimeWindows.of(Time.seconds(3)))
+
+    def countEventsByWindow: DataStream[String] = groupedEventsByWindow.process(new CountByWindowAll)
+    countEventsByWindow.print()
+    env.execute()
+
+  }
+
+
+
+  def main(args: Array[String]): Unit = {
+    demoEventTime()
+  }
+
+}
